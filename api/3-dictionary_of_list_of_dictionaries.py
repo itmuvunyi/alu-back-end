d3d7d509fd35ebf61254dd72d74f7a561ce09917
Td () <i.muvunyi@alustudent.com>
2025-07-09 10:43:35 +0200

    debug errors

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,25 +7,32 @@ if __name__ == "__main__":
     users_url = "https://jsonplaceholder.typicode.com/users"
     todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # Fetch users and tasks data
+    # Fetch users and todos data
     users = requests.get(users_url).json()
     todos = requests.get(todos_url).json()
 
+    # Prepare the data structure
     all_tasks = {}
 
     for user in users:
         user_id = user.get("id")
         username = user.get("username")
-        user_tasks = [
-            {
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            }
-            for task in todos if task.get("userId") == user_id
-        ]
+
+        # List to hold user's tasks
+        user_tasks = []
+
+        for todo in todos:
+            if todo.get("userId") == user_id:
+                task_data = {
+                    "username": username,
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed")
+                }
+                user_tasks.append(task_data)
+
+        # Add to the main dictionary
         all_tasks[user_id] = user_tasks
 
-    # Write the result to a JSON file
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_tasks, json_file)
+    # Export to JSON file
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_tasks, jsonfile)
